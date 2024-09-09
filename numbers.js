de733eb9eeb695315b24aa7e2d0626a609716859
EWART-boop <johnewartdelacruz@gmail.com>
2024-09-09 17:17:19 +0800

    Numbers.js

---
+++ numbers.js
@@ -0,0 +1,38 @@
+console.log('ADDITION');
+const firstFavNum = 19;
+const secondFavNum = 10;
+sum = (firstFavNum + secondFavNum);
+console.log('19+1 0=', sum);      
+
+
+console.log("SUBTRACTION")
+const firstFavNum1 = 19;
+const secondFavNum2 = 10;
+sub = (firstFavNum1 - secondFavNum2);
+console.log("19 - 10 = ", sub);      
+
+
+console.log('MULTIPLICATION');
+const firstFavNum2 = 19;
+const secondFavNum1 = 10;
+mul = (firstFavNum2 * secondFavNum1);
+console.log("19 * 10 ="  , mul);
+
+console.log('DIVISION');
+const firstFavNum4 = 19;
+const secondFavnum = 10;
+div = (firstFavNum4 / secondFavnum);
+console.log("19 / 10 ="  , div);
+
+console.log('MODULUS');
+const firsTfavNum = 19;
+const Secondfavnum = 10;
+modulu = (firsTfavNum % Secondfavnum);
+console.log('19 % 10 =', modulu);
+
+
+console.log('EXPONENTIATION(POWER)');
+FirstfaVnum = 19;
+const sEcondfavNum = 10;
+expo = (FirstfaVnum  ** sEcondfavNum);
+console.log("19 ** 10 =", expo);
